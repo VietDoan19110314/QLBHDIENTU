6a955965accffb66bec1a8cac9c1acf7254971eb
VietDoan19110314 <92070234+VietDoan19110314@users.noreply.github.com>
2021-11-25 14:42:00 +0700

    Add files via upload

---
+++ QLCHDoDienTu-master/Model/HoaDon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCHDoDienTu {
+	public class HoaDon {
+		public int IDHoaDon { get; set; }
+		public DateTime NgayLapHoaDon { get; set; }
+		public int IDNhanVien { get; set; }
+		public int IDKhachHang { get; set; }
+		public int IDCuaHang { get; set; }
+
+		public HoaDon(int IDHoaDon_, DateTime NgayLapHoaDon_, int IDNhanVien_, int IDKhachHang_, int IDCuaHang_) {
+			this.IDHoaDon = IDHoaDon_;
+			this.NgayLapHoaDon = NgayLapHoaDon_;
+			this.IDNhanVien = IDNhanVien_;
+			this.IDKhachHang = IDKhachHang_;
+			this.IDCuaHang = IDCuaHang_;
+		}
+		// liên kết 1-1
+		public KhachHang KHACHHANG { get; set; }
+		public NhanVien NHANVIEN { get; set; }
+		public CuaHang CUAHANG { get; set; }
+		public ChiTietHoaDon CHITIETHOADON { get; set; }
+	}
+}
