26be89b40066c000f2d54c2302dc15194b0b724d
VietDoan19110314 <92070234+VietDoan19110314@users.noreply.github.com>
2021-11-26 08:04:18 +0700

    Add files via upload

---
+++ Fixed_QLCHDoDienTu-master/Model/ChiTietHoaDon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCHDoDienTu {
+	public class ChiTietHoaDon {
+		public int IDHoaDon { get; set; }
+		public int IDSanPham { get; set; }
+		public int SoLuong { get; set; }
+		public decimal GiamGia { get; set; }
+
+		public ChiTietHoaDon(int IDHoaDon_, int IDSanPham_, int SoLuong_, decimal GiamGia_) {
+			this.IDHoaDon = IDHoaDon_;
+			this.IDSanPham = IDSanPham_;
+			this.SoLuong = SoLuong_;
+			this.GiamGia = GiamGia_;
+		}
+        //		//liên kết 1-1
+        //		public HoaDon HOADON { get; set; }
+        //		//liên kết 1-n
+        //		public ICollection<SanPham> dssanpham_chitiethoadon { get; set; }
+    }
+}
---
+++ Fixed_QLCHDoDienTu-master/Model/ThanNhan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCHDoDienTu {
+	public class ThanNhan {
+		public int IDThanNhan { get; set; }
+		public string TenThanNhan { get; set; }
+		public string QuanHe { get; set; }
+		public string GioiTinh { get; set; }
+		public DateTime NgaySinh { get; set; }
+		public int IDNhanVien { get; set; }
+
+		public ThanNhan(int IDThanNhan_, string TenThanNhan_, string QuanHe_, string GioiTinh_, DateTime NgaySinh_, int IDNhanVien_) {
+			this.IDThanNhan = IDThanNhan_;
+			this.TenThanNhan = TenThanNhan_;
+			this.QuanHe = QuanHe_;
+			this.GioiTinh = GioiTinh_;
+			this.NgaySinh = NgaySinh_;
+			this.IDNhanVien = IDNhanVien_;
+		}
+		// liên kết 1-n
+		//public ICollection<NhanVien> dsnhanvien_thannhan { get; set; }
+	}
+}
